d62f68e25aa17b4b719cfdce7b0de50a863162ef
Seandor <seandor123@gmail.com>
2016-06-05 16:40:09 +0800

    finish the logic of the game 2048

--- week1/merge.py
+++ week1/merge.py
@@ -1,31 +1,50 @@
 """
 Merge function for 2048 game.
 """
-
 def merge(line):
     """
     Function that merges a single row or column in 2048.
     """
-    temp_line = [0]*len(line);
-    index_m = 0
-    for num in line:
-    	if num != 0:
-    		temp_line[index_m] = num
-    		index_m += 1
+    def slide(line):
+        """
+        Function that slides a single row or column in 2048
+        """
+        temp_line = [0]*len(line)
+        index_s = 0
+        for num in line:
+            if num != 0:
+                temp_line[index_s] = num
+                index_s += 1
+        return temp_line
+    temp_line = slide(line)
     for index, num in enumerate(temp_line):
-    	if index < (len(temp_line)-1) and num == temp_line[index+1]:
-    		temp_line[index] += num
-    		temp_line[index+1] = 0
-    mergedline = [0]*len(temp_line);
-    index_m = 0
-    for num in temp_line:
-    	if num != 0:
-    		mergedline[index_m] = num
-    		index_m += 1
+        if index < (len(temp_line)-1) and num == temp_line[index+1]:
+            temp_line[index] += num
+            temp_line[index+1] = 0
+    mergedline = slide(temp_line)
     return mergedline
+
+# def merge(line):
+#     """
+#     Function that merges a single row or column in 2048.
+#     """
+#     temp_line = [num for num in line if num != 0]
+#     mergedline = []
+#     for index, num in enumerate(temp_line):
+#         if index < ( len(temp_line) - 1 ):
+#             if num == temp_line[index + 1]:
+#                 mergedline.append(num + num)
+#                 temp_line[index + 1] = 0
+#             elif num != 0:
+#                 mergedline.append(num)
+#         elif num != 0:
+#             mergedline.append(num)
+#     while len(mergedline) < len(line):
+#         mergedline.append(0)
+#     return mergedline
 
 print merge([2, 0, 2, 4])
 print merge([0, 0, 2, 2])
 print merge([2, 2, 0, 0])
 print merge([2, 2, 2, 2, 2])
-print merge([8, 16, 16, 8] )
+print merge([8, 16, 16, 8])
---
+++ week2/2048.py
@@ -0,0 +1,130 @@
+"""
+Clone of 2048 game.
+"""
+
+# import poc_2048_gui
+import random
+
+# Directions, DO NOT MODIFY
+UP = 1
+DOWN = 2
+LEFT = 3
+RIGHT = 4
+
+# Offsets for computing tile indices in each direction.
+# DO NOT MODIFY this dictionary.
+OFFSETS = {UP: (1, 0),
+           DOWN: (-1, 0),
+           LEFT: (0, 1),
+           RIGHT: (0, -1)}
+
+def merge(line):
+    """
+    Helper function that merges a single row or column in 2048
+    """
+    # replace with your code from the previous mini-project
+    return []
+
+class TwentyFortyEight:
+    """
+    Class to run the game logic.
+    """
+
+    def __init__(self, grid_height, grid_width):
+        # replace with your code
+        self.grid_height = grid_height
+        self.grid_width = grid_width
+        self.TILE_INDICES = {UP: [(0, col) for col in range(self.grid_width)],
+                             DOWN: [(self.grid_height-1, col) for col in range(self.grid_width)],
+                             LEFT: [(row, 0) for row in range(self.grid_height)],
+                             RIGHT: [(row, self.grid_width-1) for row in range(self.grid_height)]}
+        self.reset()
+
+    def reset(self):
+        """
+        Reset the game so the grid is empty except for two
+        initial tiles.
+        """
+        # replace with your code
+        self.grid = [[0 for col in range(self.grid_width)]
+                        for row in range(self.grid_height)]
+        self.new_tile()
+        self.new_tile()
+
+    def __str__(self):
+        """
+        Return a string representation of the grid for debugging.
+        """
+        grid_str = ''
+        for row in range(self.grid_height):
+            for col in range(self.grid_width):
+                grid_str += str(self.grid[row][col]) + ' '
+            grid_str += '\n'
+        return grid_str
+
+    def get_grid_height(self):
+        """
+        Get the height of the board.
+        """
+        return self.grid_height
+
+    def get_grid_width(self):
+        """
+        Get the width of the board.
+        """
+        return self.grid_width
+
+    def move(self, direction):
+        """
+        Move all tiles in the given direction and add
+        a new tile if any tiles moved.
+        """
+        # replace with your code
+        if direction == UP or DOWN:
+            num_steps = self.grid_height
+        else:
+            num_steps = self.grid_width
+        for start_cell in self.TILE_INDICES[direction]:
+            tmp_list = []
+            for step in range(num_steps):
+                row = start_cell[0] + step * OFFSETS[direction][0]
+                col = start_cell[1] + step * OFFSETS[direction][1]
+                tmp_list.append(self.grid[row][col])
+            tmp_list = merge(tmp_list)
+            for step in range(num_steps):
+                row = start_cell[0] + step * OFFSETS[direction][0]
+                col = start_cell[1] + step * OFFSETS[direction][1]
+                self.grid[row][col] = tmp_list[step]
+
+    def new_tile(self):
+        """
+        Create a new tile in a randomly selected empty
+        square.  The tile should be 2 90% of the time and
+        4 10% of the time.
+        """
+        while True:
+            new_row = random.randint(0, self.grid_height-1)
+            new_col = random.randint(0, self.grid_width-1)
+            if (self.grid[new_row][new_col] == 0):
+                if random.randint(0, 9) in range(0, 8):
+                    rand_num = 2
+                else:
+                    rand_num = 4
+                self.grid[new_row][new_col] =  rand_num;
+                break
+
+    def set_tile(self, row, col, value):
+        """
+        Set the tile at position row, col to have the given value.
+        """
+        self.grid[row][col] = value
+
+    def get_tile(self, row, col):
+        """
+        Return the value of the tile at position row, col.
+        """
+        return self.grid[row][col]
+
+
+print str(TwentyFortyEight(4, 4))
+# poc_2048_gui.run_gui(TwentyFortyEight(4, 4))
